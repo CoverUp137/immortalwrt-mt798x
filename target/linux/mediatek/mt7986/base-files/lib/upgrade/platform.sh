a8fa5d8e14ef5a8185854ca03904e2bf24209704
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-08-03 11:33:37 +0800

    Update platform.sh

--- target/linux/mediatek/mt7986/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/mt7986/base-files/lib/upgrade/platform.sh
@@ -148,7 +148,6 @@ platform_do_upgrade() {
 	xiaomi,redmi-router-ax6000 |\
 	bananapi,bpi-r3mini |\
 	netcore,n60 |\
-	netcore,n60-pro |\
 	ruijie,rg-x60-pro* |\
 	tplink,tl-xdr608* |\
 	zyxel,ex5700 |\
@@ -181,7 +180,6 @@ platform_check_image() {
 	xiaomi,redmi-router-ax6000* |\
 	bananapi,bpi-r3mini* |\
 	netcore,n60 |\
-	netcore,n60-pro |\
 	ruijie,rg-x60-pro* |\
 	*snand* |\
 	glinet,gl-mt6000 |\
